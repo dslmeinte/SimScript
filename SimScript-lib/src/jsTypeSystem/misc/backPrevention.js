e19586c9162fb149b27482a0ef38bc3c3f8f6e09
Meinte Boersma <meinte.boersma@gmail.com>
2014-12-14 14:28:57 +0100

    some clean-up + inclusion of jQuery-UI.datepicker

--- SimScript-lib/src/jsTypeSystem/misc/backPrevention.js
+++
@@ -1,5 +0,0 @@
-//$(window).on('beforeunload', function() {
-//	if( preventUserFromExiting != null && preventUserFromExiting() ) {
-//		return "Weet je zeker dat je deze applicatie wilt verlaten?";
-//	}
-//});
